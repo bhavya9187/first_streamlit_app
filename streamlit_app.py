7b5e267bbab58886a2faf2292563a94c18084571
bhavya9187 <86529753+bhavya9187@users.noreply.github.com>
2022-08-11 22:10:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
 
-streamlit.text(":ramen: Omega 3 & Blueberry Oatmeal")
-streamlit.text(":stew: kale, Spinach & Rocket Smoothie")
-streamlit.text(":egg: Hard-Boiled Free-Range Egg")
+streamlit.text(:ramen: 'Omega 3 & Blueberry Oatmeal')
+streamlit.text(:stew: 'kale, Spinach & Rocket Smoothie')
+streamlit.text(:egg: 'Hard-Boiled Free-Range Egg')
 
