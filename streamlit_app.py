5e695fb2c6105316ae7318bc719bb4362545e7d2
bhavya9187 <86529753+bhavya9187@users.noreply.github.com>
2022-08-19 23:33:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cur() as my_cur:
          my_cur.execute("SELECT * FROM fruit_load_list")
-         return my_data_rows = my_cur.fetchall()
+         return my_data_rows = get_fruit_load_list()
 if streamlit.button('Get Fruit Load List')
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_row)
