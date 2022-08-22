bacc1763a55b1ccc049453f51e9567aec85df83c
bhavya9187 <86529753+bhavya9187@users.noreply.github.com>
2022-08-22 11:28:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,12 +66,12 @@ streamlit.dataframe(fruityvice_normalized)
 # streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
