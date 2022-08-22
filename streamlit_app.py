0abefe97cd7f10f4b158a4653873ee78473e8cce
bhavya9187 <86529753+bhavya9187@users.noreply.github.com>
2022-08-22 22:07:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,107 +1,49 @@
-import streamlit
-import pandas
+import streamlit as st
+import pandas as pd
 import requests
 import snowflake.connector
 from urllib.error import URLError
-
-streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breakfast Favourites')
-
-streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.title("Healthy Diner")
+st.header("Breakfast Menu")
+st.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
+st.text(':green_salad: Kale, Spinach & Rocket Smoothie')
+st.text(':chicken: Hard-Boiled Free-Range Egg')
+st.header(':banana::mango: Build Your Own Fruit Smoothie :kiwifruit::grapes:')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-
-streamlit.dataframe(fruits_to_show)
-
-streamlit.header("Fruityvice Fruit Advice!")
-
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-
-def get_fruityvice_data(this_fruit_choice):
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
-     return fruityvice_normalized 
-      
-       #streamlit.write('The user entered ', fruit_choice)
-          
-          
-streamlit.header('Fruityvice Fruit Advice!')
+st.dataframe(fruits_to_show)
+def fruit_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+st.header("Fruity vice fruit advice")
 try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-   else:
-       back_from_function = get_fruityvice_data(fruit_choice)
-       streamlit.dataframe(back_from_function)
-  
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get informantion")
+  else:
+    func_data = fruit_data(fruit_choice)
+    st.dataframe(func_data)
 except URLError as e:
-     streamlit.error()
-
-#write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
-streamlit.header("The fruit load list contains:")
-def get_fruit_load_list():
-    with my_cnx.cur() as my_cur:
-         my_cur.execute("SELECT * FROM fruit_load_list")
-         return my_cur.fetchall()
-
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_row = get_fruit_load_list()
-    streamlit.dataframe(my_data_row)
-
-streamlit.write('The user entered ', add_my_fruit)
-
-# def insert_row_snowflake(new_fruit):
-#     with my_cnx.cursor() as my_cur:
-#          my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-#          return "Thanks for adding " + new_fruit 
-
-# add_my_fruit = streamlit.text_input('What fruit would you like like to add?')
-# if streamlit.button('Add a Fruit to the list'):
-#     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#     back_from_function = insert_row_snowflake(add_my_fruit)
-#     streamlit.text(back_from_function)
-          
-def insert_row_snowflake(new_fruit):
-     with my_cnx.cursor() as my_cur:
-          my_cur.execute("insert into fruit_load_list values ( 'new_fruit')")
-          return "Thanks for adding " + new_fruit 
-     
-add_my_fruit = streamlit.text_input('What fruit would you like like to add?')
-if streamlit.button('Add a Fruit to the list'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
+ st.error()
+st.header("Fruit load list")
+def get_fruit_load():
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+if st.button("Get fruit load list "):
+   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+   my_data_rows =  get_fruit_load()
+   my_cnx.close()
+   st.dataframe(my_data_rows)
+def insert_row_snowflake(nfruit):
+   with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('"+ nfruit + "')")
+      return "Thanks for adding " + nfruit
+add_user_choice = st.text_input('What fruit would you like to add?')
+if st.button("Add a fruit to list"):
+   my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+   bck_from_connector = insert_row_snowflake(add_user_choice)
+   st.text(bck_from_connector)
